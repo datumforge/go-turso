f76ca5fed2020e9d33324c9f65a9aa4a88e2df19
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2024-03-21 14:40:40 -0600

    fix config tags
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ type Config struct {
 	// Token is the token used to authenticate with the turso API
 	Token string `json:"token" koanf:"token" jsonschema:"required"`
 	// BaseURL is the base URL for the turso API
-	BaseURL string `json:"base_url" koanf:"base_url" jsonschema:"required" default:"https://api.turso.tech"`
+	BaseURL string `json:"baseUrl" koanf:"baseUrl" jsonschema:"required" default:"https://api.turso.tech"`
 	// OrgName is the name of the organization to use for the turso API
-	OrgName string `json:"org_name" koanf:"org_name" jsonschema:"required"`
+	OrgName string `json:"orgName" koanf:"orgName" jsonschema:"required"`
 }
